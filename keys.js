29a06f635d0d6b6c5c60f0578afff90c8e6f2716
Peter Ahlgren <ahlgren1234@gmail.com>
2021-07-17 16:31:06 +0200

    öjÖ

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyBpSd5vwquQXBUID1AD3yvZ97XtSjoHo1k';
+
+// Contact Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = 'a1ed1dd3f4d75b804';
