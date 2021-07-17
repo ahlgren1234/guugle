35e6c56c1e98330895c6ce0c50fbba47ce5dd697
Peter Ahlgren <ahlgren1234@gmail.com>
2021-07-17 15:47:40 +0200

    added search results

---
+++ --example--keys.js
@@ -0,0 +1,7 @@
+// API Key - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = '';
+
+// Contact Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = '';
+
+// DELETE '--example--' in the name of the file, and paste your keys.
